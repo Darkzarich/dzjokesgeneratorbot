c6732d4237cdae30b00e832db147510475ee63aa
Darkzarich <darkzarichv2@gmail.com>
2019-04-07 01:52:07 +0300

    separated files, fix worker

--- index.js
+++ index.js
@@ -1,25 +1,2 @@
-process.env["NTBA_FIX_319"] = 1;
-
-var TelegramBot = require('node-telegram-bot-api');
-
-// Устанавливаем токен, который выдавал нам бот.
-var token = '806884996:AAHj1WcKADtoxlJjsQEKcQb3votTSTBSK_g';
-// Включить опрос сервера
-var port = process.env.PORT || 8443;
-var host = process.env.HOST;
-var bot = new TelegramBot(token, {webHook: {port: port, host: host}});
-
-// Написать мне ... (/echo Hello World! - пришлет сообщение с этим приветствием.)
-bot.onText(/\/echo (.+)/, function (msg, match) {
-    var fromId = msg.from.id;
-    var resp = match[1];
-    bot.sendMessage(fromId, resp);
-});
-
-// Простая команда без параметров.
-bot.on('message', function (msg) {
-    var chatId = msg.chat.id;
-    // Фотография может быть: путь к файлу, поток(stream) или параметр file_id
-    var photo = 'cats.png';
-    bot.sendPhoto(chatId, photo, {caption: 'Милые котята'});
-});
+require('./bot');
+require('./web');
