024a6de59a5c706feaf7dec1d587d18851c0850c
Darkzarich <darkzarichv2@gmail.com>
2019-04-07 04:50:52 +0300

    change keyboard layout + added introduction

--- bot.js
+++ bot.js
@@ -6,11 +6,23 @@ const token = process.env.API_KEY;
 // Включить опрос сервера
 const bot = new TelegramBot(token, {polling: true});
 
+	const options = {
+		reply_markup: JSON.stringify({
+		    keyboard: [
+		      [{ text: 'Oh, God, I wish I knew more jokes 🙏'}],
+		    ]
+		})
+	};
+
 bot.on('callback_query', function (msg) {
 	switch (msg.data) {
-		case 'more_jokes': 
-			sendJoke(msg.from.id);
+		case 'Oh, God, I wish I got knew jokes 🙏': 
+			sendJoke(msg.from.id); break;
 	}
+});
+
+bot.onText(/\/start/, async function (msg, match) {
+	bot.sendMessage(msg.from.id, "Hello! 😁 I'm here to throw some jokes at you! 😤 Now, shall we begin? 😜 /n Feeling ready, aren't ya? Smash the button right below to start 👇👇👇", options);
 });
 
 bot.onText(/\/joke/, async function (msg, match) {
@@ -19,14 +31,6 @@ bot.onText(/\/joke/, async function (msg, match) {
 
 async function sendJoke (fromId) {
 	const {data} = await getJoke();	
-	const options = {
-		reply_markup: JSON.stringify({
-		    reply_keyboard_markup: [
-		      [{ text: 'I want more jokes!', callback_data: 'more_jokes' }],
-		    ]
-		})
-	};
-
 	bot.sendMessage(fromId, data.attachments[0].text, options);
 }
 
