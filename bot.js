56f374bd088f12dd4d845ebb57e4ed51f78f6f83
Darkzarich <darkzarichv2@gmail.com>
2019-04-07 03:14:33 +0300

    connect to api with random jokes

--- bot.js
+++ bot.js
@@ -1,10 +1,9 @@
 var TelegramBot = require('node-telegram-bot-api');
+const axios = require('axios');
 
 // Устанавливаем токен, который выдавал нам бот.
 var token = process.env.API_KEY;
 // Включить опрос сервера
-var port = process.env.PORT || 8443;
-var host = process.env.HOST;
 const bot = new TelegramBot(token, {polling: true});
 
 // Написать мне ... (/echo Hello World! - пришлет сообщение с этим приветствием.)
@@ -14,10 +13,22 @@ bot.onText(/\/echo (.+)/, function (msg, match) {
     bot.sendMessage(fromId, resp);
 });
 
-// Простая команда без параметров.
-bot.on('message', function (msg) {
-    var chatId = msg.chat.id;
-    // Фотография может быть: путь к файлу, поток(stream) или параметр file_id
-    var photo = 'cats.png';
-    bot.sendPhoto(chatId, photo, {caption: 'Милые котята'});
+// // Простая команда без параметров.
+// bot.on('message', function (msg) {
+//     var chatId = msg.chat.id;
+//     // Фотография может быть: путь к файлу, поток(stream) или параметр file_id
+//     var photo = 'cats.png';
+//     bot.sendPhoto(chatId, photo, {caption: 'Милые котята'});
+// });
+
+bot.onText(/\/joke (.+)/, async function (msg, match) {
+	let config = {
+	  headers: {
+	    header1: 'Accept: application/json',
+	  }
+	}
+
+	let {data} = await axios.get("https://icanhazdadjoke.com/", config);
+	var fromId = msg.from.id;	
+	bot.sendMessage(fromId, data.attachments[0].text);
 });
